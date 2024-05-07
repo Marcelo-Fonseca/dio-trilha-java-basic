0b36fbfd056592a9c741c1e0f6205dacdd291ae6
Marcelo Fonseca <projetosweb11@gmail.com>
2024-05-07 16:09:18 -0300

    Criado os TODOS o que deve ser feito

--- conta-banco/src/contaTerminal.java
+++ conta-banco/src/contaTerminal.java
@@ -1,5 +1,8 @@
 public class contaTerminal {
     public static void main(String[] args) throws Exception {
-       
+       //TODO: Conhecer e importar as classe Scanner
+       //Exibir as mensagens para o nosso usuario
+       //obeter pela classe Scanner os valores digitados pelo usuario
+      //Exibir a mensagem da conta criada
     }
 }
