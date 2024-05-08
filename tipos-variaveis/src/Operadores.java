bd3ed748d683285bc96f626df9e77fa67ef3429f
Marcelo Fonseca <projetosweb11@gmail.com>
2024-05-08 17:25:20 -0300

    atualização

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,24 @@
+public class Operadores {
+
+    //Operadores são simbolos especiais
+    //que utiliza em operações e expressoes matematica. 
+
+    //Operadores aritimeticos
+    //operadores unários
+    //operadores relacionais
+    //operadores lógicos
+    //operadores ternarios
+
+    //---------operadores----------
+    //operador    Comparação
+    // ==   igual   
+    //!=    Diferente
+    //<     Menor
+    //>     Maior
+    //<=   Menor e igual
+    //>=   Maior e igual 
+    
+    // = ATribuição
+    // === igual e do mesmo tipo 
+}   // "" algo dentro de "" Representa String
+    // . 
