a0532d99d5fbc7f2523e2885aafdf22e15141b58
Marcelo Fonseca <projetosweb11@gmail.com>
2024-05-09 09:41:56 -0300

    exemplos

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -21,4 +21,11 @@ public class Operadores {
     // = ATribuição
     // === igual e do mesmo tipo 
 }   // "" algo dentro de "" Representa String
-    // . 
+    // . ponto é usado quando um valor tem separação ex 47.47 não se usa virgula.
+    
+    //exemplo
+    //String nome = "Gleyson";
+    //int idade = 22;
+    //double peso 68.5;
+    //boolean doador Orgao = false;
+    //Date dataNascimento = new Date();
