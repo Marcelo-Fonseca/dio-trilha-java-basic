0860b00e76dedc3e5884721db19651a6f4540660
Marcelo Fonseca <projetosweb11@gmail.com>
2024-06-04 18:57:01 -0300

    PlanoOeradora usando switch /case

---
+++ controle-de-fluxo/src/PlanoOperadora.java
@@ -0,0 +1,28 @@
+public class PlanoOperadora {
+    public static void main(String[] args) {
+        String plano = "j"; //M/T
+
+        switch(plano) {
+            case "T": {
+                System.out.println("5GB youtube");
+                break;
+
+            }
+            case "M":{
+                System.out.println("Whats e instagram gratis");
+                break;
+            }
+
+            case "B": {
+                System.out.println("100 minutos de ligação");
+                break;
+            }
+            default:
+            System.out.println("Plano não exite favor verificar!!");
+
+        }
+
+
+    }
+    
+}
